933bd9ccd164b07dab70b2642b71b992a7fe0b4a
anandarahayu22 <anandarahayu967@gmail.com>
2024-05-12 20:20:34 +0700

    responsive ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'list_anime',
+      title: 'List_Anime',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
